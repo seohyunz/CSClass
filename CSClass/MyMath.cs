afb24e0168bdcb94cae542cec16ea7615745df28
seohyunz <joseohyun0201@gmail.com>
2022-06-07 10:59:09 +0900

    #9 클래스 메서드 인스턴스 변수,메드는 클래스 메서드에서 사용이 불가하다

---
+++ CSClass/MyMath.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CSClass
+{
+
+    internal class MyMath
+    {
+
+        int some_int=3;
+
+         public static int Abs(int v) //절대값을 구하는 매서드
+        {
+            //some_int = 5;  인스턴스 변수 : 클래스 메서드에서 사용 불가
+            //foo();         인스턴스 메서드 : 클래스 메서드에서 사용 불가
+            return (v < 0) ? -1 : v;
+        }
+
+        void foo()
+        {
+            Console.WriteLine("클래스 메서드에서 접근 불가");
+        }
+
+
+
+    }
+}
--- CSClass/Program.cs
+++ CSClass/Program.cs
@@ -105,6 +105,9 @@ namespace CSClass
             }
 
 
+            Console.WriteLine(MyMath.Abs(52);
+            Console.WriteLine(MyMath.Abs(52);
+
 
         }
     }
